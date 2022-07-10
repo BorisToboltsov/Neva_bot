ccce6306c733a814516f6100a41101bffa5ebb81
Gh0st184 <tu5s0qg>
2022-07-10 20:56:26 +0300

    add access

---
+++ access.py
@@ -0,0 +1,29 @@
+import settings
+from connect_gsheets import connect
+
+
+def check_access(user_id):
+    sheet_access = connect(settings.SHEET_ACCESS, "Доступ",
+                           ['A', 'B', 'C'])[0]
+
+    full_access = {}
+    sales_access = {}
+    suppliers_access = {}
+    for cell in sheet_access:
+        if cell[2].value == "FULL_ACCESS":
+            full_access[cell[1].value] = [cell[0].value, cell[2].value]
+        elif cell[2].value == "SALES_ACCESS":
+            sales_access[cell[1].value] = [cell[0].value, cell[2].value]
+        elif cell[2].value == "SUPPLIERS_ACCESS":
+            suppliers_access[cell[1].value] = [cell[0].value, cell[2].value]
+
+    if str(user_id) in full_access:
+        check = "FULL_ACCESS"
+    elif str(user_id) in sales_access:
+        check = "SALES_ACCESS"
+    elif str(user_id) in suppliers_access:
+        check = "SUPPLIERS_ACCESS"
+    else:
+        check = "NOT_ACCESS"
+
+    return check
--- menu_telegram.py
+++ menu_telegram.py
@@ -1,13 +1,23 @@
 import settings
 from keyboard_telegram import keyboard
+from access import check_access
 
 
 def menu(user_id):
     """Основное меню"""
-    if str(user_id) in settings.ACCESS.values():
+    check = check_access(user_id)
+
+    if check == "FULL_ACCESS":
         main_menu = keyboard(
             ['Заполнить питание', 'Заполнить транспорт', 'Отправить сообщение, транспорт',
              'Отправить сообщение, туристы', 'Показать мой telegram_id'])
+    elif check == "SALES_ACCESS":
+        main_menu = keyboard(
+            ['Отправить сообщение, туристы', 'Показать мой telegram_id'])
+    elif check == "SUPPLIERS_ACCESS":
+        main_menu = keyboard(
+            ['Заполнить питание', 'Заполнить транспорт', 'Отправить сообщение, транспорт',
+             'Показать мой telegram_id'])
     else:
         main_menu = keyboard(['Показать мой telegram_id'])
     return main_menu
