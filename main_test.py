9d986d4505d6170839f83ecd0a365bd2a9483459
Gh0st184 <tu5s0qg>
2022-07-10 14:13:06 +0300

    init

---
+++ main_test.py
@@ -0,0 +1,18 @@
+# import settings
+# from connect_gsheets import connect
+#
+# """
+# 'D' - Название Экскурсии
+# 'E' - Поставщик
+# 'F' - Дата экскурсии
+# 'G' - Место сбора
+# 'H' - Фио туристов
+# 'I' - Моб.номер
+# 'J' - Кол-во туристов
+# 'P' - Гид
+# 'Q' - Телефон гида
+# 'R' - Статус
+# """
+# sheet_excursions = connect(settings.SHEET_EXCURSIONS, "Продажи 2022",
+#                            ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'P', 'Q', 'R'])
+# print(sheet_excursions)
